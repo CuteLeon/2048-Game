b5d8b64ddbd14b657ec75b5b988b66103e983a8a
Leon <xiaoyankeji@hotmail.com>
2016-10-03 10:18:31 +0800

    第一次提交

---
+++ 2048 Game/GameForm.Designer.vb
@@ -0,0 +1,58 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class GameForm
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(GameForm))
+        Me.ScoreLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ScoreLabel
+        '
+        Me.ScoreLabel.BackColor = System.Drawing.Color.Transparent
+        Me.ScoreLabel.Font = New System.Drawing.Font("微软雅黑", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.ScoreLabel.ForeColor = System.Drawing.Color.Gray
+        Me.ScoreLabel.Location = New System.Drawing.Point(275, 38)
+        Me.ScoreLabel.Name = "ScoreLabel"
+        Me.ScoreLabel.Size = New System.Drawing.Size(115, 40)
+        Me.ScoreLabel.TabIndex = 0
+        Me.ScoreLabel.Text = "0"
+        Me.ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'GameForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(400, 540)
+        Me.Controls.Add(Me.ScoreLabel)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "GameForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ScoreLabel As Label
+End Class
